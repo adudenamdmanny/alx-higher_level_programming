776d9fc7cec46d1bfb95742de22f93c0c094c776
adudenamdmanny <emmanueljames.essien@gmail.com>
2023-07-25 14:49:57 -0700

    Star Wars

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,32 @@
 #!/usr/bin/node
 const request = require('request');
-const id = process.argv[2];
-request('http://swapi.co/api/films/' + id + '/', function (error, response, body) {
-  if (error == null) {
-    const json = JSON.parse(body);
-    console.log(json.title);
+
+// Function to get the Star Wars movie title based on the provided movie ID
+function getStarWarsMovieTitle(movieID) {
+  const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
+
+  request(url, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      const movie = JSON.parse(body);
+      console.log(`Title of Episode ${movie.episode_id}: ${movie.title}`);
+    } else {
+      console.error('Error fetching data from the Star Wars API');
+    }
+  });
+}
+
+// Check if the script is executed from the command line
+if (require.main === module) {
+  // Get the movie ID from the command line arguments
+  const movieID = process.argv[2];
+
+  if (!movieID || isNaN(movieID)) {
+    console.error('Please provide a valid movie ID (integer) as the first argument.');
+    process.exit(1);
   }
-});
+
+  getStarWarsMovieTitle(movieID);
+}
+
+module.exports = getStarWarsMovieTitle;
+
