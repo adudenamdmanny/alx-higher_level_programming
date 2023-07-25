2673c96431d3658f0fb2bd6e0f3f87d0243dbcc8
Emmanuel Essien <117782578+adudenamdmanny@users.noreply.github.com>
2023-07-25 23:05:04 +0100

    Update 3-starwars_title.js

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,32 +1,16 @@
 #!/usr/bin/node
-const request = require('request');
 
-// Function to get the Star Wars movie title based on the provided movie ID
-function getStarWarsMovieTitle(movieID) {
-  const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
+// prints the title of a Star Wars movie by movie ID
+const rq = require('request');
 
-  request(url, (error, response, body) => {
-    if (!error && response.statusCode === 200) {
-      const movie = JSON.parse(body);
-      console.log(`Title of Episode ${movie.episode_id}: ${movie.title}`);
-    } else {
-      console.error('Error fetching data from the Star Wars API');
-    }
-  });
-}
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-// Check if the script is executed from the command line
-if (require.main === module) {
-  // Get the movie ID from the command line arguments
-  const movieID = process.argv[2];
-
-  if (!movieID || isNaN(movieID)) {
-    console.error('Please provide a valid movie ID (integer) as the first argument.');
-    process.exit(1);
+rq(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const movie = JSON.parse(body);
+    console.log(movie.title);
   }
-
-  getStarWarsMovieTitle(movieID);
-}
-
-module.exports = getStarWarsMovieTitle;
-
+});
