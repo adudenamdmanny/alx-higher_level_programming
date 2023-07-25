5c22c9ea09b6838095e70216f440c4634107fa3e
Emmanuel Essien <117782578+adudenamdmanny@users.noreply.github.com>
2023-07-25 23:16:16 +0100

    Update 101-starwars_characters.js

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,30 +1,32 @@
 #!/usr/bin/node
-const request = require('request');
-const url = 'http://swapi.co/api/films/';
-let id = parseInt(process.argv[2], 10);
-let characters = [];
 
-request(url, function (err, response, body) {
-  if (err == null) {
-    const resp = JSON.parse(body);
-    const results = resp.results;
-    if (id < 4) {
-      id += 3;
-    } else {
-      id -= 3;
-    }
-    for (let i = 0; i < results.length; i++) {
-      if (results[i].episode_id === id) {
-        characters = results[i].characters;
-        break;
-      }
-    }
-    for (let j = 0; j < characters.length; j++) {
-      request(characters[j], function (err, response, body) {
-        if (err == null) {
-          console.log(JSON.parse(body).name);
-        }
-      });
-    }
+// prints all characters of a Star Wars movie in the right order
+const req = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
+
+req(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    const charactersUrls = JSON.parse(body).characters;
+    printCharactersNames(charactersUrls, 0);
   }
 });
+
+function printCharactersNames (charactersUrls, index) {
+  if (index >= charactersUrls.length) {
+    return;
+  }
+
+  req(charactersUrls[index], (error, response, body) => {
+    if (error) {
+      console.error(error);
+    } else {
+      const characterName = JSON.parse(body).name;
+      console.log(characterName);
+      printCharactersNames(charactersUrls, index + 1);
+    }
+  });
+}
